4445dc4a0cc03997025c147e0e53a1fe28b082f2
Xijun95 <xijunw95@gmail.com>
2018-02-22 16:16:58 -0600

    Add files via upload

---
+++ js/my_ikigai.js
@@ -0,0 +1,48 @@
+﻿$(function () {
+    var paid = (sessionStorage.hasOwnProperty("paid") ? sessionStorage.getItem("paid") : "nothing").split(",");
+    var good = (sessionStorage.hasOwnProperty("paid") ? sessionStorage.getItem("paid") : "nothing").split(",");
+    var need = (sessionStorage.hasOwnProperty("paid") ? sessionStorage.getItem("paid") : "nothing").split(",");
+    var love = (sessionStorage.hasOwnProperty("paid") ? sessionStorage.getItem("paid") : "nothing").split(",");
+    $("#paid")[0].innerHTML = paid[0];
+    $("#good")[0].innerHTML = good[0];
+    $("#need")[0].innerHTML = need[0];
+    $("#love")[0].innerHTML = love[0];
+
+    $("#paid_info").css("display", "block");
+    $("#good_info").css("display", "none");
+    $("#need_info").css("display", "none");
+    $("#love_info").css("display", "none");
+    $("#paid_info")[0].innerHTML = "Paid for: " + paid;
+
+    $("#paid").mousemove(function (e) {
+        $("#paid_info").css("display", "block");
+        $("#good_info").css("display", "none");
+        $("#need_info").css("display", "none");
+        $("#love_info").css("display", "none");
+        $("#paid_info")[0].innerHTML = "Paid for: " + paid;
+    });
+
+    $("#good").mousemove(function (e) {
+        $("#paid_info").css("display", "none");
+        $("#good_info").css("display", "block");
+        $("#need_info").css("display", "none");
+        $("#love_info").css("display", "none");
+        $("#good_info")[0].innerHTML = "Good at: " + good;
+    });
+
+    $("#need").mousemove(function (e) {
+        $("#paid_info").css("display", "none");
+        $("#good_info").css("display", "none");
+        $("#need_info").css("display", "block");
+        $("#love_info").css("display", "none");
+        $("#need_info")[0].innerHTML = "World need: " + need;
+    });
+
+    $("#love").mousemove(function (e) {
+        $("#paid_info").css("display", "none");
+        $("#good_info").css("display", "none");
+        $("#need_info").css("display", "none");
+        $("#love_info").css("display", "block");
+        $("#love_info")[0].innerHTML = "You love: " + love;
+    });
+})
--- js/questions.js
+++ js/questions.js
@@ -1,13 +1,51 @@
 var responseCounter = 0;
 
-window.onload = function() {
+window.onload = function () {
     $( "#add" ).click(function() {
         var div = document.createElement("div");
         div.className = "response-item";
         div.id = "item" + responseCounter;
         
         var divContent = document.createElement("p");
-        var input = document.getElementById("input").value;
+        //var input = document.getElementById("input").value;
+        var input = "";
+        if (document.getElementById("input-paid")) {
+            input = $("#input-paid").val();
+            $("#input-paid").val("");
+            var paid = new Array();
+            if (sessionStorage.hasOwnProperty("paid")) {
+                paid.push(sessionStorage.getItem("paid"));
+            }
+            paid.push(input);
+            sessionStorage.setItem("paid", paid);
+        } else if (document.getElementById("input-good")) {
+            input = $("#input-good").val();
+            $("#input-good").val("");
+            var good = new Array();
+            if (sessionStorage.hasOwnProperty("good")) {
+                good.push(sessionStorage.getItem("good"));
+            }
+            good.push(input);
+            sessionStorage.setItem("good", good);
+        } else if (document.getElementById("input-need")) {
+            input = $("#input-need").val();
+            $("#input-need").val("");
+            var need = new Array();
+            if (sessionStorage.hasOwnProperty("need")) {
+                need.push(sessionStorage.getItem("need"));
+            }
+            need.push(input);
+            sessionStorage.setItem("need", need);
+        } else {
+            input = $("#input-love").val();
+            $("#input-love").val("");
+            var love = new Array();
+            if (sessionStorage.hasOwnProperty("love")) {
+                love.push(sessionStorage.getItem("love"));
+            }
+            love.push(input);
+            sessionStorage.setItem("love", love);
+        }
         divContent.innerHTML = input;
         div.appendChild(divContent);
         
@@ -20,7 +58,7 @@ window.onload = function() {
         
         document.getElementById("responses").appendChild(div);
         
-        $("input").val("");
+        //$("input").val("");
         responseCounter ++;
     });
 }
