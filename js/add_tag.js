893d00a95d317fda120a863be67aafd259422c7a
Xijun95 <xijunw95@gmail.com>
2018-03-02 17:23:42 +0800

    Add files via upload

---
+++ js/add_tag.js
@@ -0,0 +1,18 @@
+﻿$("#submit_tag").click(function (e) {
+    var input = $("#tagname").val();
+    var typeArray = new Array("paid", "good", "need", "love");
+    for (var i = 0; i < 4; i++) {
+        if (document.getElementById(typeArray[i]).checked) {
+            var temp = new Array();
+            if (sessionStorage.hasOwnProperty(typeArray[i])) {
+                temp.push(sessionStorage.getItem(typeArray[i]));
+            }
+            temp.push(input);
+            sessionStorage.setItem(typeArray[i], temp);
+        }
+    }
+
+    window.location.href = "my_ikigai.html";
+});
+
+
