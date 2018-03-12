fe3e9c2d23086cebe0b3d63192623a11a322a1e3
spenserhuang <spenserhuang515@gmail.com>
2018-03-12 04:27:30 -0500

    My ikigai

--- js/my_ikigai.js
+++ js/my_ikigai.js
@@ -3,23 +3,23 @@
     var good = (sessionStorage.hasOwnProperty("good") ? sessionStorage.getItem("good") : "nothing").split(",");
     var need = (sessionStorage.hasOwnProperty("need") ? sessionStorage.getItem("need") : "nothing").split(",");
     var love = (sessionStorage.hasOwnProperty("love") ? sessionStorage.getItem("love") : "nothing").split(",");
-    $("#paid")[0].innerHTML = paid[0];
-    $("#good")[0].innerHTML = good[0];
-    $("#need")[0].innerHTML = need[0];
-    $("#love")[0].innerHTML = love[0];
+        
+    $("#paid-circle")[0].innerHTML = paid[0];
+    $("#good-circle")[0].innerHTML = good[0];
+    $("#need-circle")[0].innerHTML = need[0];
+    $("#love-circle")[0].innerHTML = love[0];
 
     $("#paid_info").css("display", "block");
     $("#good_info").css("display", "none");
     $("#need_info").css("display", "none");
     $("#love_info").css("display", "none");
-    $("#paid_info")[0].innerHTML = "Paid for: " + paid;
 
     $("#paid").mousemove(function (e) {
         $("#paid_info").css("display", "block");
         $("#good_info").css("display", "none");
         $("#need_info").css("display", "none");
         $("#love_info").css("display", "none");
-        $("#paid_info")[0].innerHTML = "Paid for: " + paid;
+        $("#paid_info")[0].innerHTML = paid;
     });
 
     $("#good").mousemove(function (e) {
@@ -27,7 +27,7 @@
         $("#good_info").css("display", "block");
         $("#need_info").css("display", "none");
         $("#love_info").css("display", "none");
-        $("#good_info")[0].innerHTML = "Good at: " + good;
+        $("#good_info")[0].innerHTML = good;
     });
 
     $("#need").mousemove(function (e) {
@@ -35,7 +35,7 @@
         $("#good_info").css("display", "none");
         $("#need_info").css("display", "block");
         $("#love_info").css("display", "none");
-        $("#need_info")[0].innerHTML = "World need: " + need;
+        $("#need_info")[0].innerHTML = need;
     });
 
     $("#love").mousemove(function (e) {
@@ -43,6 +43,6 @@
         $("#good_info").css("display", "none");
         $("#need_info").css("display", "none");
         $("#love_info").css("display", "block");
-        $("#love_info")[0].innerHTML = "You love: " + love;
+        $("#love_info")[0].innerHTML = love;
     });
 })
